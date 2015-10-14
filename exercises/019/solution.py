73e9686b5cf08ed3bc61cd8ed40c1cb6b82b8cd0
Nina <nina.varchavsky@cri-paris.org>
2015-10-14 15:36:07 +0200

    adin

---
+++ exercises/019/solution.py
@@ -0,0 +1,2 @@
+import time
+print time.strftime('Today is %Y and it is %H:%M:%S',time.localtime()) 
---
+++ exercises/025/solution.py
@@ -0,0 +1,2 @@
+import time
+print time.strftime('Today is %Y-%B-%d and it is %H:%M:%S',time.localtime())
