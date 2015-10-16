adc57291f17c4432df726361ecd0eb19958ad243
Nina <nina.varchavsky@cri-paris.org>
2015-10-16 16:49:28 +0200

    adin

---
+++ exercises/060/solution.py
@@ -0,0 +1,5 @@
+list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+for lettre1 in list:
+    for lettre2 in list:
+        print(lettre1,lettre2)
