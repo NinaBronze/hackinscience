8ab1424a86c62eacf341cb57e58b12f2957c104d
Nina <nina.varchavsky@cri-paris.org>
2015-10-16 17:08:02 +0200

    shiem

--- exercises/060/solution.py
+++ exercises/060/solution.py
@@ -1,6 +1,4 @@
-list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
+list = "abcdefghijklmnopqrstuvwxyz"
 for lettre1 in list:
     for lettre2 in list:
         print(lettre1 + lettre2)
