c2c534002046ab99fb58eb96669885c5ac1f98e7
Nina <nina.varchavsky@cri-paris.org>
2015-10-16 14:54:51 +0200

    tri

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -1,4 +1,7 @@
 import sys
-a = int(sys.argv[1]) 
-b = int(sys.argv[2])
-print(a + b) 
+if len(sys.argv) > 12:
+    a = int(sys.argv[1]) 
+    b = int(sys.argv[2])
+    print(a + b) 
+else:
+    print("usage: python3 solution.py OP1 OP2")
