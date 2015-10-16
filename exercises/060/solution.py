9b73a5939e7250a67e4d34ae0aefde60bb31a28d
Nina <nina.varchavsky@cri-paris.org>
2015-10-16 16:51:33 +0200

    dva

--- exercises/060/solution.py
+++ exercises/060/solution.py
@@ -1,5 +1,7 @@
-list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+list = ["a", "b", "c", "d", "e", "f", "g",\
+ "h", "i", "j", "k", "l", "m", "n", "o", "p",\
+ "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 for lettre1 in list:
     for lettre2 in list:
-        print(lettre1,lettre2)
+        print(lettre1, lettre2)
