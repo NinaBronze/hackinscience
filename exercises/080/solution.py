7fb68fe308dc5f0aacf50dbd595d327f6e3a79c0
Nina <nina.varchavsky@cri-paris.org>
2015-10-18 18:36:34 +0200

    dva

--- exercises/080/solution.py
+++ exercises/080/solution.py
@@ -1,8 +1,6 @@
 list = "abcdefghijklmnopqrstuvwxyz"
-
 for lettre1 in list:
     for lettre2 in list:
         if lettre1 != lettre2:
             print(lettre1 + lettre2)
     list = list[1::]
-    
