17a2ff381e7ae0cecc1a994975426c44292b65e0
Nina <nina.varchavsky@cri-paris.org>
2015-10-21 22:55:43 +0200

    adin

---
+++ exercises/085/solution.py
@@ -0,0 +1,14 @@
+import operator
+
+def sort_a_list(l):
+    a = sorted(l)
+    print(a)
+
+def sort_by_mark(my_class):
+    a = sorted(my_class, key=operator.itemgetter(0), reverse=True)
+    print(a)
+
+
+def sort_by_name(my_class):
+    a = sorted(my_class, key=operator.itemgetter(1))
+    print(a)
