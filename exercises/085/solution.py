8db803f6c5728b0a5a6174434dc7196e995a625d
Nina <nina.varchavsky@cri-paris.org>
2015-10-23 12:28:26 +0200

    tchetyre

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -3,14 +3,14 @@ import operator
 
 def sort_a_list(l):
     a = sorted(l, reverse=True)
-    print(a)
+    return(a)
 
 
 def sort_by_mark(my_class):
     a = sorted(my_class, key=operator.itemgetter(0), reverse=True)
-    print(a)
+    return(a)
 
 
 def sort_by_name(my_class):
     a = sorted(my_class, key=operator.itemgetter(1))
-    print(a)
+    return(a)
