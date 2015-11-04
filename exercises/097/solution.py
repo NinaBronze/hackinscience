53c07dcba3832381279c8d36cc41b5c5b0345c2f
Nina <nina.varchavsky@cri-paris.org>
2015-11-04 19:17:53 +0100

    adin

---
+++ exercises/097/solution.py
@@ -0,0 +1,22 @@
+def love_meet(amant1, amant2):
+    rencontre = []
+    for i in amant1:
+        for j in amant2:
+            if i == j:
+                rencontre.append(i)
+    rencontre = set(rencontre)
+    print(rencontre)
+
+
+def affair_meet(amant1, amant2, amant3):
+    rencontre = []
+    for i in amant2:
+        for j in amant3:
+            if i == j:
+                rencontre.append(i)
+    for k in rencontre:
+        for l in amant1:
+            if k == l:
+                rencontre.remove(k)
+    rencontre = set(rencontre)
+    print(rencontre)
