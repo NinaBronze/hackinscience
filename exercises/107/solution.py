9f52df43e6b6bcf99dddfffa5e20d4adbc5af113
Nina <nina.varchavsky@cri-paris.org>
2015-11-06 19:02:10 +0100

    dva

--- exercises/107/solution.py
+++ exercises/107/solution.py
@@ -9,7 +9,7 @@ def select_student(classe, note):
             bon.append(i)
         else:
             mauvais.append(i)
-    dico = {'Accepted' : \
+    dico = {'Accepted': \
             sorted(bon, key=operator.itemgetter(1), reverse=True),
-            'Refused' : sorted(mauvais, key=operator.itemgetter(1))}
+            'Refused': sorted(mauvais, key=operator.itemgetter(1))}
     return(dico)
