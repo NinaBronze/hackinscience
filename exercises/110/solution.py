a4dda9e2d4b5c1ec309df9f2a9ddcbdd7612c6d7
Nina <nina.varchavsky@cri-paris.org>
2015-11-07 21:02:37 +0100

    adin

---
+++ exercises/110/solution.py
@@ -0,0 +1,38 @@
+import sys
+
+if len(sys.argv) < 4:
+    print("usage: solution.py a_number (an_operator +-*/%^) a_number")
+
+else:
+    a = sys.argv[1]
+    op = sys.argv[2]
+    b = sys.argv[3]
+
+    try:
+        a = int(a)
+        b = int(b)
+    except ValueError:
+        print('input error')
+        sys.exit()
+
+    if op == '+':
+        print(a + b)
+
+    if op == '-':
+        print(a - b)
+
+    if op == '*':
+        print(a * b)
+
+    if op == '/':
+        if b == 0:
+            print('input error')
+            sys.exit()
+        else:
+            print(a / b)
+
+    if op == '**':
+        print(a ** b)
+
+    if op == '%':
+        print(a % b)
