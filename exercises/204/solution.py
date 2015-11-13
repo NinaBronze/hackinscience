ae93c9cbaaab769698ffc3a672c4fa6fa7db0982
Nina <nina.varchavsky@cri-paris.org>
2015-11-13 16:19:20 +0100

    adin

---
+++ exercises/204/solution.py
@@ -0,0 +1,14 @@
+def perfect_shuffle(deck):
+    newdeck = []
+    num = len(deck)
+    milieu = num // 2
+    deck1 = deck[0:milieu]
+    deck2 = deck[milieu::]
+    for i in range(0, milieu):
+        a = deck1[i]
+        b = deck2[i]
+        newdeck.append(a)
+        newdeck.append(b)
+    if len(deck1) != len(deck2):
+        newdeck.append(deck2[-1])
+    print(newdeck)
