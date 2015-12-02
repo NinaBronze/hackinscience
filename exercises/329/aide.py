35b1a8b23493cb487ce6d19e57deee8a7f3f7f41
Nina <nina.varchavsky@cri-paris.org>
2015-12-02 22:46:15 +0100

    dva

--- exercises/329/aide.py
+++ exercises/329/aide.py
@@ -1,6 +1,5 @@
 def multi(c):
     result = 1
-    c = c.split("\n")
     for i in c:
         i = int(i)
         result = result * i
--- exercises/329/solution.py
+++ exercises/329/solution.py
@@ -3,11 +3,8 @@ from aide import *
 fichier = open('test.txt', 'r')
 suite_nb = fichier.read()
 le_plus_grand = 0
-for i in range(0, len(suite_nb)):
+for i in range(0, len(suite_nb) - 13):
     a = multi(suite_nb[i:(i + 13)])
-    b = multi(suite_nb[(i + 1):(i + 14)])
-    if a * b != 0 and a < b:
-        le_plus_grand = multi(suite_nb[(i + 1):(i + 14)])
-    else:
-        le_plus_grand = multi(suite_nb[i:(i + 13)])
+    if a > le_plus_grand:
+        le_plus_grand = a
 print(le_plus_grand)
