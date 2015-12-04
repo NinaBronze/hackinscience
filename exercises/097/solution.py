9c494c42eea7103b79f2f2a9f1ca5dedd4e13ba7
Nina <nina.varchavsky@cri-paris.org>
2015-12-04 15:24:50 +0100

    vosiem

--- exercises/097/solution.py
+++ exercises/097/solution.py
@@ -9,11 +9,7 @@ def love_meet(amant1, amant2):
 
 
 def affair_meet(amant1, amant2, amant3):
-    rencontre = []
-    for i in amant1:
-        for j in amant2:
-            for k in amant3:
-                if i != j and k == j:
-                    rencontre.append(i)
-    rencontre = set(rencontre)
+    dispute = set(amant2) & set(amant1)
+    amour = set(amant2) & set(amant3)
+    rencontre = amour - dispute
     return(rencontre)
