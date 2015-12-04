399ee546d2b15c1d0dcacbc99355ac27a2505c22
Nina <nina.varchavsky@cri-paris.org>
2015-12-04 15:56:39 +0100

    dva

--- exercises/330/solution.py
+++ exercises/330/solution.py
@@ -1,7 +1,8 @@
 import json
 
 
-def load_json(jason):
-    op = open(jason, 'r')
+def load_json(fichier):
+    op = open(fichier, 'r')
     lire = op.read()
-    return(lire)
+    my_json = json.loads(lire)
+    return(my_json)
