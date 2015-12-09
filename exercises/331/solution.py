8a37f1998ca3a2997f185dff98300e0d627636c3
Nina <nina.varchavsky@cri-paris.org>
2015-12-09 14:57:49 +0100

    chest

--- exercises/331/solution.py
+++ exercises/331/solution.py
@@ -1,17 +1,29 @@
 import json
+from aide import *
 
 station = open("velib.json", 'r')
 velib = json.load(station)
 solution = open("solution.json", 'w')
+a = 0
 
+#On passe en revue toutes les 1230 stations!!!!
 for i in velib:
     NAME = i["name"].split(" - ")
-    ADDRESS = i["address"].split("- ")
-    VILLE = ADDRESS[-1].split(" ")
-    i["zip_code"] = VILLE[0]
-    i["name"] = NAME[1]
-    i["address"] = ADDRESS[0]
-    i["city"] = VILLE[-1]
+    i["name"] = " ".join(NAME[1::])
+
+    ADDRESS = sans_tiret_sans_espace(i["address"])
+    for d in ADDRESS:
+        if len(d) == 5:
+            try:
+                numero = ADDRESS.index(d)
+                d = int(d)
+                i["zip_code"] = d
+                i["city"] = ' '.join(ADDRESS[(numero + 1)::])
+                i["address"] = ' '.join(ADDRESS[0:numero])
+                print(i["address"])
+            except ValueError:
+                None
+    print(i)
 
 json.dump(velib, solution)
 solution.close()
