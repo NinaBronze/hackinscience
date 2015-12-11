01c0ecd0fa360084d1066ea7eb99e2ba603131d0
Nina <nina.varchavsky@cri-paris.org>
2015-12-11 11:54:26 +0100

    dieviat

--- exercises/331/solution.py
+++ exercises/331/solution.py
@@ -4,7 +4,7 @@ from aide import *
 station = open("velib.json", 'r')
 velib = json.load(station)
 solution = open("solution.json", 'w')
-a = 0
+
 
 for i in velib:
     NAME = i["name"].split(" - ")
@@ -15,7 +15,7 @@ for i in velib:
             try:
                 numero = ADDRESS.index(d)
                 d = int(d)
-                i["zip_code"] = d
+                i["zip_code"] = str(d)
                 i["city"] = ' '.join(ADDRESS[(numero + 1)::])
                 i["address"] = ' '.join(ADDRESS[0:numero])
             except ValueError:
