996b7d924dde60520a3b944bf173e638e625bc73
Nina <nina.varchavsky@cri-paris.org>
2015-12-13 19:12:40 +0100

    adinatiate

--- exercises/331/aide.py
+++ exercises/331/aide.py
@@ -1,5 +1,8 @@
 def sans_tiret_sans_espace(phrase):
-    sans_tiret = phrase.split("- ") 
+    sans_tiret = phrase.split("- ")
+    if len(sans_tiret) > 2:
+        sans_tiret[0] = "- ".join(sans_tiret[0:2])
+        del sans_tiret[1]
     phrase_2_0 = " ".join(sans_tiret)
     sans_espace = phrase_2_0.split(" ")
     for i in sans_espace:
