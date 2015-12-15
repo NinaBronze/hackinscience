da0690c74a65d5bfdd75ff3f4bc02eb5a8d78bb5
Nina <nina.varchavsky@cri-paris.org>
2015-12-15 10:53:36 +0100

    dvenatiate

--- exercises/331/solution.py
+++ exercises/331/solution.py
@@ -1,5 +1,18 @@
 import json
-from aide import *
+
+
+def sans_tiret_sans_espace(phrase):
+    sans_tiret = phrase.split("- ")
+    if len(sans_tiret) > 2:
+        sans_tiret[0] = "- ".join(sans_tiret[0:2])
+        del sans_tiret[1]
+    phrase_2_0 = " ".join(sans_tiret)
+    sans_espace = phrase_2_0.split(" ")
+    for i in sans_espace:
+        if i == '':
+            sans_espace.remove(i)
+    return(sans_espace)
+
 
 station = open("velib.json", 'r')
 velib = json.load(station)
