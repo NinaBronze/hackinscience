b9321fb785fa745317d3de8bb5d9f1f3049f9179
Nina <nina.varchavsky@cri-paris.org>
2016-01-04 16:55:32 +0100

    adin

--- exercises/440/solution.py
+++ exercises/440/solution.py
@@ -3,7 +3,7 @@
 def filtered(items, filtre):
     new_items = []
     for i in items:
-        if filtre(i) == True:
+        if filtre(i):
             new_items.append(i)
     for j in new_items[0:len(new_items)-1]:
         print(j, end=',')
