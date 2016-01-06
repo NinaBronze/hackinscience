75b6d1d0c717ced4b181c4da670ff749231009f9
Nina <nina.varchavsky@cri-paris.org>
2016-01-06 13:37:53 +0100

    tchetyre

--- exercises/440/solution.py
+++ exercises/440/solution.py
@@ -6,11 +6,11 @@ def filtered(items, filtre):
         if filtre(i):
             new_items.append(i)
     for j in new_items[0:len(new_items)-1]:
-        a = print(j, end=', ')
-    a = print(new_items[len(new_items)-1])
-    return(a)
+        print(j, end=', ')
+    print(new_items[len(new_items)-1])
+    return()
 
 if __name__ == '__main__':
-    trois = filtered(range(0, 101), lambda x: x % 3 == 0)
-    cinq = filtered(range(0, 101), lambda x: x % 5 == 0)
-    quinze = filtered(range(0, 101), lambda x: x % 15 == 0)
+    filtered(range(0, 101), lambda x: x % 3 == 0)
+    filtered(range(0, 101), lambda x: x % 5 == 0)
+    filtered(range(0, 101), lambda x: x % 15 == 0)
