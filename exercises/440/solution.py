0fac2ac05e94bbc535dae1da78a0bc1f3762ba13
Nina <nina.varchavsky@cri-paris.org>
2016-01-08 17:50:20 +0100

    voila

--- exercises/440/solution.py
+++ exercises/440/solution.py
@@ -2,15 +2,16 @@
 
 def filtered(items, filtre):
     new_items = []
+    a = ''
     for i in items:
         if filtre(i):
             new_items.append(i)
     for j in new_items[0:len(new_items)-1]:
-        print(j, end=', ')
-    print(new_items[-1])
-    return()
+        a = a + str(j) + ', '
+    a = a + str(new_items[-1])
+    return(a)
 
 if __name__ == '__main__':
-    filtered(range(0, 101), lambda x: x % 3 == 0)
-    filtered(range(0, 101), lambda x: x % 5 == 0)
-    filtered(range(0, 101), lambda x: x % 15 == 0)
+    print(filtered(range(0, 101), lambda x: x % 3 == 0))
+    print(filtered(range(0, 101), lambda x: x % 5 == 0))
+    print(filtered(range(0, 101), lambda x: x % 15 == 0))
